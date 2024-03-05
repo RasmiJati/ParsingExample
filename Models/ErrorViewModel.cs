cd2e38b1b7ed0faeeb1311bba82a4653ff5d763d
Rasmi Jati <jatirasmi65@gmail.com>
2024-03-05 16:48:34 +0545

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ParseAndNavigation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
